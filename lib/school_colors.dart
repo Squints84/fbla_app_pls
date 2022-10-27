44d24525f25970523f1778c6d9c6becfed5f6da9
Cole G <116569259+cgolding8@users.noreply.github.com>
2022-10-26 21:42:29 -0400

    School Colors Update

--- lib/school_colors.dart
+++ lib/school_colors.dart
@@ -5,9 +5,9 @@ class SchoolColors{
   static const int f = 255;
 
   static const Color UCVTS = Color.fromARGB(f, 28, 40, 154);
-  static const Color Allied = Color.fromARGB(f, 98, 11, 19);
+  static const Color Allied = Color.fromARGB(f, 121, 41, 46);
   static const Color AIT = Color.fromARGB(f, 248, 184, 16);
   static const Color APA = Color.fromARGB(f, 175, 179, 172);
   static const Color Magnet = Color.fromARGB(f, 149, 195, 222);
-  static const Color UCTech = Color.fromARGB(f, 252, 194, 126);
+  static const Color UCTech = Color.fromARGB(f, 255, 211, 110);
 }
